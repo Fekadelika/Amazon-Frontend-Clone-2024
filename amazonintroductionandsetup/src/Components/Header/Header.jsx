e798417ef3a7853e879f0f90fa37673424306feb
Fekadelika <fekade.wolde@yahoo.com>
2024-09-22 13:02:39 -0400

    cart addition update

--- amazonintroductionandsetup/src/Components/Header/Header.jsx
+++ amazonintroductionandsetup/src/Components/Header/Header.jsx
@@ -11,74 +11,75 @@ import { DataContext } from "../DataProvider/DataProvider";
 const Header = () => {
 
 const [{basket},dispatch] = useContext(DataContext)
+const totalItem = basket?.reduce((amount, item) => {
+  return item.amount + amount;
+}, 0);
   return (
-    
-      <section className={classes.fixed}>
-        <section>
-          <div className={classes.header_container}>
-            {/* Logo and Delivery section */}
-            <div className={classes.logo_container}>
-              <Link to="/">
-                <img
-                  src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                  alt="amazon logo"
-                />
-              </Link>
-              <div className={classes.delivery}>
-                <span>
-                  <SlLocationPin />
-                </span>
-                <div>
-                  <p>Delivered to</p>
-                  <span>Ethiopia</span>
-                </div>
+    <section className={classes.fixed}>
+      <section>
+        <div className={classes.header_container}>
+          {/* Logo and Delivery section */}
+          <div className={classes.logo_container}>
+            <Link to="/">
+              <img
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="amazon logo"
+              />
+            </Link>
+            <div className={classes.delivery}>
+              <span>
+                <SlLocationPin />
+              </span>
+              <div>
+                <p>Delivered to</p>
+                <span>Ethiopia</span>
               </div>
             </div>
-
-            {/* Search section */}
-            <div className={classes.search}>
-              <select>
-                <option value="">All</option>
-              </select>
-              <input type="text" placeholder="Search product" />
-              <BsSearch size={25} />
-            </div>
-
-            {/* Orders, Language, Cart Section */}
-            <div className={classes.order_container}>
-              <Link to="#" className={classes.language}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Hopkinson_Flag_of_the_United_States_Navy.svg/330px-Hopkinson_Flag_of_the_United_States_Navy.svg.png"
-                  alt="US Flag"
-                />
-                <select>
-                  <option value="">EN</option>
-                </select>
-              </Link>
-
-              {/* Account Sign In */}
-              <Link to="#">
-                <p>Sign In</p>
-                <span>Account & Lists</span>
-              </Link>
-
-              {/* Orders Section */}
-              <Link to="/orders">
-                <p>returns</p>
-                <span>& Orders</span>
-              </Link>
-
-              {/* Cart Section */}
-              <Link to="/cart" className={classes.cart}>
-                <BiCart size={35} />
-                <span>{basket.length}</span>
-              </Link>
-            </div>
           </div>
-        </section>
-        <LowerHeader />
+
+          {/* Search section */}
+          <div className={classes.search}>
+            <select>
+              <option value="">All</option>
+            </select>
+            <input type="text" placeholder="Search product" />
+            <BsSearch size={25} />
+          </div>
+
+          {/* Orders, Language, Cart Section */}
+          <div className={classes.order_container}>
+            <Link to="#" className={classes.language}>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Hopkinson_Flag_of_the_United_States_Navy.svg/330px-Hopkinson_Flag_of_the_United_States_Navy.svg.png"
+                alt="US Flag"
+              />
+              <select>
+                <option value="">EN</option>
+              </select>
+            </Link>
+
+            {/* Account Sign In */}
+            <Link to="#">
+              <p>Sign In</p>
+              <span>Account & Lists</span>
+            </Link>
+
+            {/* Orders Section */}
+            <Link to="/orders">
+              <p>returns</p>
+              <span>& Orders</span>
+            </Link>
+
+            {/* Cart Section */}
+            <Link to="/cart" className={classes.cart}>
+              <BiCart size={35} />
+              <span>{totalItem}</span>
+            </Link>
+          </div>
+        </div>
       </section>
-    
+      <LowerHeader />
+    </section>
   );
 };
 
