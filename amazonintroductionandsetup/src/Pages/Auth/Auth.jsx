53b8af5f576769c2729f180b48075b0990c7b938
Fekadelika <fekade.wolde@yahoo.com>
2024-09-22 20:50:28 -0400

    Auth sign in page

--- amazonintroductionandsetup/src/Pages/Auth/Auth.jsx
+++ amazonintroductionandsetup/src/Pages/Auth/Auth.jsx
@@ -16,6 +16,32 @@ function Auth() {
       </Link>
 
       {/* form  */}
+      <div className={classes.login__container}>
+        <h1>Sign In</h1>
+        <form action="">
+          <div>
+
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+
+          </div>
+          <div>
+
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+
+         
+            <button className={classes.login__signInButton}>Sign In</button>
+         
+        </form>
+{/* agreement */}
+<p>By continuing, you agree to Amazon fake clone's Conditions of Use and Privacy Notice.</p>
+{/* create account */}
+<button className={classes.login__registerButton}>Create your Amazon Account</button>
+
+
+      </div>
     </section>
   );
 }
