eb7c14c9c136621d944c9108ddbff900e1796cc3
Fekadelika <fekade.wolde@yahoo.com>
2024-09-23 09:10:16 -0400

    sign in page aut

--- amazonintroductionandsetup/src/Pages/Auth/Auth.jsx
+++ amazonintroductionandsetup/src/Pages/Auth/Auth.jsx
@@ -3,6 +3,7 @@ import classes from './SignUp.module.css'
 import { Link } from 'react-router-dom';
 import {auth} from '../../Utility/firebase'
 import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth'
+import { ClipLoader } from "react-spinners";
 import {DataContext} from '../../Components/DataProvider/DataProvider';
 import { Type } from '../../Utility/action.type';
 
@@ -12,6 +13,10 @@ function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error,setError] = useState("");
+  const [loading,setLoading] = useState
+({signIn:false,
+  signup:false
+});
   // console.log(password,email);
 
   const [{user},dispatch] = useContext(DataContext)
@@ -23,32 +28,35 @@ const authHandler = async(e) => {
 
   if (e.target.name == "signin") {
     // firebase aut
+    setLoading({...loading,signIn:true})
     signInWithEmailAndPassword(auth, email, password)
       .then((userInfo) => {
        
         dispatch({
           type: Type.SET_USER,
-          user: userInfo.user,})
+          user: userInfo.user,
+        })
+           setLoading({ ...loading, signIn: true });
         
       })
       .catch((error) => {
-        console.log(error);
+        setError(error.message);
+        setLoading({ ...loading, signIn: false });
       });
     
   }else{
+     setLoading({ ...loading, signup: true });
 createUserWithEmailAndPassword(auth, email, password).then((userInfo) => {
   
   dispatch({
     type: Type.SET_USER,
     user: userInfo.user,
   });
+  setLoading({ ...loading, signup: false });
 }).catch((error) => {
-  console.log(error);
+  setError(error.message);
+  setLoading({ ...loading, signup: false });
 })
-
-
-
-
 
 
   }
@@ -72,31 +80,58 @@ createUserWithEmailAndPassword(auth, email, password).then((userInfo) => {
         <h1>Sign In</h1>
         <form action="">
           <div>
-
             <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" />
-
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+            />
           </div>
           <div>
-
             <label htmlFor="password">Password</label>
-            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+            />
           </div>
 
-         
-            <button type="submit" onClick={authHandler} 
+          <button
+            type="submit"
+            onClick={authHandler}
             name="signIn"
-            className={classes.login__signInButton}>Sign In</button>
-         
+            className={classes.login__signInButton}
+          >
+            {loading.signIn ? (
+              <ClipLoader color="#000" size={15}></ClipLoader>
+            ) : (
+              "signIn"
+            )}
+          </button>
         </form>
-{/* agreement */}
-<p>By continuing, you agree to Amazon fake clone's Conditions of Use and Privacy Notice.</p>
-{/* create account */}
-<button type="submit" 
-name="signUp"
-onClick={authHandler} className={classes.login__registerButton}>Create your Amazon Account</button>
-
-
+        {/* agreement */}
+        <p>
+          By continuing, you agree to Amazon fake clone's Conditions of Use and
+          Privacy Notice.
+        </p>
+        {/* create account */}
+        <button
+          type="submit"
+          name="signUp"
+          onClick={authHandler}
+          className={classes.login__registerButton}
+        >
+          {loading.signUp ? (
+            <ClipLoader color="#000" size={15}></ClipLoader>
+          ) : (
+            "Create your Amazon Account"
+          )}
+        </button>
+        {error && (
+          <small style={{ paddingTop: "5px", color: "red" }}>{error}</small>
+        )}
       </div>
     </section>
   );
