b1c05a93ee4323047c5019cf1753b16cd5b988c7
Fekadelika <fekade.wolde@yahoo.com>
2024-09-25 05:24:04 -0400

    Payment

--- amazonintroductionandsetup/src/Router.jsx
+++ amazonintroductionandsetup/src/Router.jsx
@@ -7,7 +7,12 @@ import Orders from "./Pages/Orders/Orders";
 import Cart from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results";
 import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
+const stripePromise = loadStripe(
+  "pk_test_51Q2OryFZwtbJzV8SrE5jM0Wvd0Jve3ClE7yfxDiMK7YW3geNmhIjBMKsVAlEmOjoJOAm7Q8ccV55Bzs8OjuRxG4200xgcSUlsK"
+);
 
 function Routing() {
   return (
@@ -15,12 +20,18 @@ function Routing() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/payment" element={<Payment />} />
+        <Route
+          path="/payments"
+          element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          }
+        />
         <Route path="/orders" element={<Orders />} />
         <Route path="/category/:categoryName" element={<Results />} />
         <Route path="/products/:productId" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
-     
       </Routes>
     </Router>
   );
