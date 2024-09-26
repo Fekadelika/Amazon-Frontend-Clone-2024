8eaeda6d535653a56fdf052243031291d2e4ff44
Fekadelika <fekade.wolde@yahoo.com>
2024-09-26 00:09:04 -0400

    payment linking

--- amazonintroductionandsetup/src/Api/axios.jsx
+++
@@ -1,7 +0,0 @@
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: "https://amazon-backend-updated.onrender.com",
-// });
-
-// export { axiosInstance };
--- amazonintroductionandsetup/src/Pages/Payment/Payment.jsx
+++ amazonintroductionandsetup/src/Pages/Payment/Payment.jsx
@@ -1,34 +1,79 @@
-import React,{useContext,useState} from 'react'
-import classes from './Payment.module.css'
-import LayOut from '../../LayOut/LayOut'
-import { DataContext } from '../../Components/DataProvider/DataProvider'
-import ProductCard from "../../Components/Product/ProductCard"
-import {
-  useStripe,
-  useElements,
-  CardElement
-  } from "@stripe/react-stripe-js";
-import CurrencyFormat from '../../Components/CurrencyFormat/CurrencyFormat'
-
-
+import React, { useContext, useState } from "react";
+import classes from "./Payment.module.css";
+import LayOut from "../../LayOut/LayOut";
+import { DataContext } from "../../Components/DataProvider/DataProvider";
+import ProductCard from "../../Components/Product/ProductCard";
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormat";
+import { axiosInstance } from "../../Api/axios";
+import {ClipLoader} from 'react-spinners'
+import { db } from "../../Utility/firebase";
+import { useNavigate } from "react-router-dom";
 
 function Payment() {
-  const [{ user, basket }, dispatch] = useContext(DataContext);
+  const [{ user, basket }] = useContext(DataContext);
   console.log(user);
-const totalItem = basket?.reduce((amount, item) => {
-  return item.amount + amount;
-}, 0);
- const total = basket.reduce((amount, item) => {
-   return item.price * item.amount + amount;
- }, 0);
-const [cardError,setCardError] = useState(null)
-const stripe = useStripe();
-const elements = useElements();
+  const totalItem = basket?.reduce((amount, item) => {
+    return item.amount + amount;
+  }, 0);
 
-const handleChange=(e)=>{
-  console.log(e);
-  e?.error?.message? setCardError(e?.error?.message):setCardError("")
-};
+  const total = basket.reduce((amount, item) => {
+    return item.price * item.amount + amount;
+  }, 0);
+  const [cardError, setCardError] = useState(null);
+  const [processing, setProcessing] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    // console.log(e);
+    e?.error?.message ? setCardError(e?.error?.message) : setCardError("");
+  };
+
+  const handlePayment = async (e) => {
+    e.preventDefault();
+
+    try {
+      setProcessing(true);
+      // 1 backend || functions ---->contact to the client secret
+      const response = await axiosInstance({
+        method: "POST",
+        url: `payment/create?total=${total * 100}`,
+      });
+      // console.log(response.data);
+      const clientSecret = response.data?.clientSecret;
+
+      //2client side (react side confirmation)
+
+      const paymentIntent = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      });
+
+      // console.log(paymentIntent);
+
+      //3. after the confirmation--> order  database clear the basket
+          await db
+            .collection("users")
+            .doc(user.uid)
+            .collection("orders")
+            .doc(paymentIntent.id)
+            .set({
+              basket: basket,
+              amount: paymentIntent.amount,
+              created: paymentIntent.created,
+            });
+          // empty the basket
+          // dispatch({ type: Type.EMPTY_BASKET });
+      setProcessing(false);
+      navigate("/orders", {state:{msg:"Order Placed Successfully"}});
+    } catch (err) {
+      console.log(err);
+      setProcessing(false);
+    }
+  };
 
   return (
     <LayOut>
@@ -62,10 +107,10 @@ const handleChange=(e)=>{
           <h3>Payment methods</h3>
           <div className={classes.payment__card__container}>
             <div className={classes.payment__details}>
-              <form action="">
+              <form onSubmit={handlePayment}>
                 {/* error */}
                 {cardError && (
-                  <small style={{ color: "red" }}>{cardError} </small>
+                  <small style={{ color: "red" }}>{cardError}</small>
                 )}
                 {/* card element */}
                 <CardElement onChange={handleChange} />
@@ -73,11 +118,20 @@ const handleChange=(e)=>{
                 {/* price */}
                 <div className={classes.payment__price}>
                   <div>
-                    <span style={{ display: "flex", gap:"10px" }}>
-                      <p>Total Order</p> | <CurrencyFormat amount={total} />
+                    <span style={{ display: "flex", gap: "10px" }}>
+                      <p>Total Order |</p> <CurrencyFormat amount={total} />
                     </span>
                   </div>
-                  <button>Pay Now</button>
+                  <button type="submit">
+                    {processing ? (
+                      <div className={classes.loading}>
+                        <ClipLoader color="gray" size={12} />
+                        <p>Please wait ...</p>
+                      </div>
+                    ) : (
+                      "Pay Now"
+                    )}
+                  </button>
                 </div>
               </form>
             </div>
@@ -88,4 +142,4 @@ const handleChange=(e)=>{
   );
 }
 
-export default Payment
+export default Payment;
